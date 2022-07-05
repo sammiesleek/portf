c27faed5597f710cf743fa4b11026195ff835cdf
sammiesleek <samlocomotive1@gmail.com>
2022-07-05 10:22:05 +0100

    .

--- admin/js/Hproject.js
+++ admin/js/Hproject.js
@@ -1,25 +1,19 @@
-
-const Wrapper = document.getElementById('my_projects');
+const Wrapper = document.getElementById("my_projects");
 // const err = document.querySelector('.err');
 
-
-
-
-setInterval(() => {
-    
-     let xhr = new XMLHttpRequest;
-    xhr.open("GET","admin/controller/Hproject.php", true);
-    xhr.onload = ()=>{
-        if(xhr.readyState == XMLHttpRequest.DONE){
-            if(xhr.status === 200){
-                let data = xhr.response;
-                Wrapper.innerHTML=data;
-                
-            }
-
-        }
+const ProjectLoader = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "admin/controller/Hproject.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        Wrapper.innerHTML = data;
+      }
     }
+  };
 
-    xhr.send()
+  xhr.send();
+};
 
-}, 10000);
+ProjectLoader();
--- admin/js/mg_projects.js
+++ admin/js/mg_projects.js
@@ -1,25 +1,19 @@
-
-const Wrapper = document.querySelector('.pro_wrap');
+const Wrapper = document.querySelector(".pro_wrap");
 // const err = document.querySelector('.err');
 
-
-
-
-setInterval(() => {
-    
-     let xhr = new XMLHttpRequest;
-    xhr.open("GET","controller/projects.php", true);
-    xhr.onload = ()=>{
-        if(xhr.readyState == XMLHttpRequest.DONE){
-            if(xhr.status === 200){
-                let data = xhr.response;
-                Wrapper.innerHTML=data;
-                
-            }
-
-        }
+const Loadpro = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "controller/projects.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        Wrapper.innerHTML = data;
+      }
     }
+  };
 
-    xhr.send()
+  xhr.send();
+};
 
-}, 500);
+Loadpro();
